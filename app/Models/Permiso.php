ae083aed5afa58cab0dab25e7073674192caa8b6
jlramirez17 <jlramirez17@gmail.com>
2020-02-21 16:17:33 -0400

    creados seeder, modelos, datos en tablas rol, permiso

---
+++ app/Models/Permiso.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Permiso extends Model
+{
+    protected $table = 'permiso'; //le indica a laravel el nombre de la tabla con la que trabajará éste modelo
+}
